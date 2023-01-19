017cb2bdffc64daa9a5f34c4d74c1a9ca42b7344
jhammad <javier.hammad@gmail.com>
2023-01-19 11:55:26 +0000

    First stage of the Ravenous app - Business , BusinessList and SearchBar components created

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar'; 
+import BusinessList from '../BusinessList/BusinessList';
+
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+  <h1>ravenous</h1>
+    <SearchBar />
+    <BusinessList />
+  </div>    
+  );
+}
+
+export default App;
